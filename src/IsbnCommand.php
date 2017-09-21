882e6e1ffc091d5e981a6f070bc8f2f55c4f3aed
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-09-21 17:07:35 +0200

    Fix hyphen

--- src/IsbnCommand.php
+++ src/IsbnCommand.php
@@ -27,7 +27,7 @@ class IsbnCommand extends Command
         ->setDescription('Generate a list of ISBNs')
         ->addArgument('source', InputArgument::REQUIRED, 'The delimiter-separated source file you want to process')
         ->addArgument('destination', InputArgument::REQUIRED, 'The name of the tab separated file for the SFX Dataloader')
-        ->addOption('delimiter', null, InputOption::VALUE_REQUIRED, 'Set a custom delimiter if the auto detection doesn\t work')
+        ->addOption('delimiter', null, InputOption::VALUE_REQUIRED, 'Set a custom delimiter if the auto detection doesn\'t work')
         ->addOption('status', null, InputOption::VALUE_REQUIRED, 'Set the desired status for the portfolios (e.g. ACTIVE, INACTIVE, NONE)', 'ACTIVE')
         ->addOption('force', null, InputOption::VALUE_NONE, 'Overwrite the destination file if it already exists');
     }
